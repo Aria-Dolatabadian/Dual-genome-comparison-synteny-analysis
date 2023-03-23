11aa99d3a48af38f51781882f55494142cb773c6
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-23 09:10:31 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -6,5 +6,8 @@ head(data)
 
 table(data$species)
 
+data(synteny_dual_comparison, package="RIdeogram")
+head(synteny_dual_comparison)
+
 ideogram(karyotype = data, synteny = synteny_dual_comparison)
 convertSVG("chromosome.svg", device = "png")
