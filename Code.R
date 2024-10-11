76dda2be9b685d776365f41552bff5b00076b58f
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2024-10-11 15:03:04 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,13 +1,21 @@
 library(RIdeogram)
 
+# Load the karyotype data
 data <- read.csv("karyotype_dual_comparison.csv")
-
 head(data)
 
+# Check species in the dataset
 table(data$species)
 
-data(synteny_dual_comparison, package="RIdeogram")
+# Load the synteny dual comparison data
+data(synteny_dual_comparison, package = "RIdeogram")
 head(synteny_dual_comparison)
 
+# Export synteny_dual_comparison as a CSV file
+write.csv(synteny_dual_comparison, "synteny_dual_comparison.csv", row.names = FALSE)
+
+# Create the ideogram
 ideogram(karyotype = data, synteny = synteny_dual_comparison)
+
+# Convert the ideogram to PNG
 convertSVG("chromosome.svg", device = "png")
